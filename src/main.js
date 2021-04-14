ae570cc249d4ad342dba4fe07ff36d5930e67675
boeschricht <boeschricht@gmail.com>
2021-04-14 23:26:21 +0200

    j

--- src/main.js
+++ src/main.js
@@ -35,7 +35,26 @@ Apify.main(async () => {
         navigationTimeout = 30000,
     } = input;
 
-    // use or create a named key-value store for historic data
+    const dataset = await Apify.openDataset('Kurser20210414');
+    dataset.pushData({date: Date_toISOStringLocal(today), time: Time_toISOStringLocal(today), key1: "url1", val1: content1, key2: "url2", val2: content2, key3: "url3", val3: content3})
+    log.info('Closing Puppeteer...');
+    await browser.close();
+    
+    const client = await Apify.newClient();
+    const datasetClient = client.datasetClient('jrnKQ29nVrrWuz7KS');
+    // const datasetClient = await Apify.client.dataset('jrnKQ29nVrrWuz7KS');
+    const datasetHTML = await datasetClient.downloadItems("html")
+    const datasetXLSX = await datasetClient.downloadItems("xlsx")
+    await Apify.call('apify/send-mail', {
+        to: 'boeschricht@gmail.com;boeschricht@gmail.com',
+        subject: 'Kurser på obligationer',
+        html: datasetHTML, 
+        attachments: [{
+            filename: 'Kurser '+ Date_toISOStringLocal(today) +' '+ Time_toISOStringLocal(today) + '.xlsx',
+            data: datasetXLSX 
+        }]
+    });
+
     var today = new Date();
    
     const proxyConfiguration = await Apify.createProxyConfiguration(proxy);
@@ -113,25 +132,6 @@ Apify.main(async () => {
     }
     log.info(`url3 data: ${content3}`);
     
-    const dataset = await Apify.openDataset('Kurser20210414');
-    dataset.pushData({date: Date_toISOStringLocal(today), time: Time_toISOStringLocal(today), key1: "url1", val1: content1, key2: "url2", val2: content2, key3: "url3", val3: content3})
-    log.info('Closing Puppeteer...');
-    await browser.close();
-    
-    const client = await Apify.newClient();
-    const datasetClient = client.dataset('jrnKQ29nVrrWuz7KS');
-    // const datasetClient = await Apify.client.dataset('jrnKQ29nVrrWuz7KS');
-    const datasetHTML = await datasetClient.downloadItems("html")
-    const datasetXLSX = await datasetClient.downloadItems("xlsx")
-    await Apify.call('apify/send-mail', {
-        to: 'boeschricht@gmail.com;boeschricht@gmail.com',
-        subject: 'Kurser på obligationer',
-        html: datasetHTML, 
-        attachments: [{
-            filename: 'Kurser '+ Date_toISOStringLocal(today) +' '+ Time_toISOStringLocal(today) + '.xlsx',
-            data: datasetXLSX 
-        }]
-    });
     
     log.info('Done.');
 });
