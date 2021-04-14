da955c087932946ebbd39681accd8a3a2cd8ed12
boeschricht <boeschricht@gmail.com>
2021-04-14 13:28:45 +0200

    s

--- src/validate-input.js
+++ src/validate-input.js
@@ -1,7 +1,7 @@
 module.exports = (input) => {
     // check inputs
-    if (!input || !input.url || !input.contentSelector || !input.sendNotificationTo) {
+    if (!input || !input.url1 || !input.contentSelector1 || !input.url2 || !input.contentSelector2 || !input.url3 || !input.contentSelector3) {
         throw new Error('Invalid input, must be a JSON object with the '
-        + '"url", "contentSelector", "screenshotSelector" and "sendNotificationTo" field!');
+        + '"url1", "contentSelector1", "url1", "contentSelector1", "url1", "contentSelector1"');
     }
 };
