58bc38abd7366047b2944a9874ae86307d65aacf
boeschricht <boeschricht@gmail.com>
2021-04-16 10:13:43 +0200

    k

--- src/main.js
+++ src/main.js
@@ -44,7 +44,8 @@ Apify.main(async () => {
     const ApifyClient = require('apify-client');
     apifyClient = new ApifyClient({ token: 'uAqFSRMzpGuFCkRb8fjX77tni' });
     log.info('Client: ' + apifyClient);
-    const datasetClient = apifyClient.dataset('boeschricht/Kurser20210414');
+    // const datasetClient = apifyClient.dataset('boeschricht/Kurser20210414');
+    const datasetClient = apifyClient.dataset('jrnKQ29nVrrWuz7KS');
     const datasetHTML = await datasetClient.downloadItems("html");
     const datasetXLSX = await datasetClient.downloadItems("xlsx");
     await Apify.call('apify/send-mail', {
